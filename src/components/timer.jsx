daa454a0fa7e76ae9675bad235de0a272df911c8
Nandan R Pai <51937330+nandanpi@users.noreply.github.com>
2023-06-08 08:56:09 +0530

    Init Commit

---
+++ src/components/timer.jsx
@@ -0,0 +1,145 @@
+import React , {useEffect , useState} from "react";
+
+const Timer = () => {
+    const [isRunning , setIsRunning] = useState(false);
+    const [pomores , setPomores] = useState(false);
+    const [timerLengths , setTimerLengths] = useState(25*60);
+    const [currentTimer , setCurrentTimer] = useState(timerLengths);
+    const [prevTimer, setPrevTimer] = useState(0);
+    const [nextTimer, setNextTimer] = useState(5*60);
+    const [showReset , setShowReset] = useState(false);
+    const [currentSessionNumber, setCurrentSessionNumber] = useState(1);
+    const [prevSessionNumber, setPrevSessionNumber] = useState(1);
+    const [nextSessionNumber, setNextSessionNumber] = useState(1);
+    const [currentRestNumber, setCurrentRestNumber] = useState(1);
+    const [prevRestNumber, setPrevRestNumber] = useState(1);
+    const [nextRestNumber, setNextRestNumber] = useState(1);
+    const [showPermission, setShowPermission] = useState(false);
+    let interval = null;
+    function toggleTimer() {
+        setIsRunning(!isRunning);
+    }
+
+    function resetTimer() {
+        setCurrentTimer(timerLengths);
+        setIsRunning(false);
+        setShowReset(false)
+    }
+    function inc(){
+        if(currentTimer !== 0){
+            if(!isRunning){
+                setTimerLengths(currentTimer + 60);
+                setCurrentTimer(currentTimer+60);
+            }
+            else
+                setCurrentTimer(currentTimer+60);
+        }
+    }
+    function dec(){
+        if(!(currentTimer<=60)){
+            if(!isRunning){
+                setTimerLengths(currentTimer - 60);
+                setCurrentTimer(currentTimer - 60);
+            }
+            else
+                setCurrentTimer(currentTimer-60);
+        }
+    }
+    function pad(d){
+        return (d < 10) ? '0' + d.toString() : d.toString();
+    }
+    useEffect(() => {
+        if(isRunning){
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            interval = setInterval(() => {
+                setCurrentTimer((seconds) => seconds - 1);
+            }, 1000);
+        } else if (!isRunning && currentTimer !== 0) {
+            clearInterval(interval);
+        }
+        if(currentTimer < timerLengths)
+            setShowReset(true);
+        if(currentTimer === 0){
+            setPomores(!pomores);
+            setPrevTimer(currentTimer);
+            setCurrentTimer(nextTimer);
+            setNextTimer(nextTimer === 10 ? 30 : 10);
+            if (nextTimer === 10) {
+                setCurrentSessionNumber((prevNumber) => prevNumber + 1);
+            } else {
+                setCurrentRestNumber((prevNumber) => prevNumber + 1);
+            }
+            setPrevSessionNumber(currentSessionNumber);
+            setCurrentSessionNumber(nextSessionNumber);
+            setNextSessionNumber(currentSessionNumber + 1);
+            setPrevRestNumber(currentRestNumber);
+            setCurrentRestNumber(nextRestNumber);
+            setNextRestNumber(currentRestNumber + 1);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning, pomores, currentTimer, timerLengths, nextTimer, currentSessionNumber, nextSessionNumber, currentRestNumber, nextRestNumber, prevSessionNumber, prevRestNumber]);
+
+    useEffect(() => {
+        if(currentRestNumber !== 1){
+            setShowPermission(true);
+            clearInterval(interval);
+        }
+    },[currentRestNumber, interval])
+    function Continue(){
+        console.log("Continue");
+        setShowPermission(false);
+        setIsRunning(!isRunning);
+    }
+    function notContinue(){
+        console.log("Not Continue");
+        setShowPermission(false);
+        setCurrentTimer(timerLengths);
+        setIsRunning(false);
+        setShowReset(false);
+        setCurrentSessionNumber(1);
+        setCurrentRestNumber(1);
+        setPrevSessionNumber(1);
+        setPrevRestNumber(1);
+        setNextSessionNumber(1);
+        setNextRestNumber(1);
+    }
+    return(
+        <>
+            <div className={currentSessionNumber && nextSessionNumber === 1 ? "hidden" : "block"}>
+                {pad(Math.floor(prevTimer/60))}:{pad(prevTimer%60)}
+                {nextTimer !== 10 ? <span>Session {prevSessionNumber}</span> : <span>Rest {prevRestNumber}</span>}
+            </div>
+            <div>
+                {pad(Math.floor(currentTimer/60))}:{pad(currentTimer%60)}
+                {nextTimer === 10 ? <span>Session {currentSessionNumber}</span> : <span>Rest {currentRestNumber}</span>}
+            </div>
+            <div>
+                {pad(Math.floor(nextTimer/60))}:{pad(nextTimer%60)}
+                {nextTimer !== 10 ? <span>Session {nextSessionNumber}</span> : <span>Rest {nextRestNumber}</span>}
+            </div>
+            <div>
+                <button onClick={toggleTimer}>{isRunning ? 'Stop' : 'Start'}</button>
+            </div>
+            <div>
+                <button onClick={resetTimer} className={showReset ? 'block' : 'hidden'}>Reset</button>
+            </div>
+            <div>
+                <button onClick={inc}>+</button>
+            </div>
+            <div>
+                <button onClick={dec}>-</button>
+            </div>
+            <div className={showPermission ? "block" : "hidden"}>
+                <button onClick={Continue}>
+                    Yes
+                </button>
+                <button onClick={notContinue}>
+                    No
+                </button>
+            </div>
+
+        </>
+
+    )
+}
+export default Timer;
